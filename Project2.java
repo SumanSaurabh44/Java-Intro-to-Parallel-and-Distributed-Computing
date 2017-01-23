b02c3afc4929af34defe58184fc604c37fdfa8b8
Suman Saurabh <sumansaurabh44@gmail.com>
2017-01-23 18:33:40 -0500

    Create Project2.java
    Write two programs to chat. Each program is playing the role of a client and a server. Name one of them MyServer.java and the other MyClient.java. This is how these programs should work:
    1. Run MyServer. The program is waiting for the client. After the client is connected the program send: Hello !!
    2. Run MyClient. This program receives The word: Hello !!.
    The following steps are repeated until the client enters: bye.
    Step I) Enter a message on the client’s console (Like: How are you server?) This message should appear on the server’s console (like: How are you server?)
    Step II) Enter a message on the server’s console (like: I am good. Do you have any question client?). This message should appear on the client’s console (like: I am good. Do you have any question client?)
    
    These two steps should be repeated until the client enters: bye.

---
+++ Project2.java
@@ -0,0 +1,74 @@
+/**************************************************My Server*****************************************************/
+//MyServer :
+
+import java.io.*;
+import java.net.*;
+
+public class MyServer {
+	public static void main(String[] args) throws IOException {
+
+		try {
+			int port = 16790;
+			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket clientSocket = serverSocket.accept();
+			System.out.println("server started"); 
+			String message = "Client started\n";
+			while (true) {
+				DataOutputStream dataOut = new DataOutputStream(clientSocket.getOutputStream());
+				dataOut.writeUTF(message); 
+				DataInputStream dataIn = new DataInputStream(clientSocket.getInputStream());
+				String clientMessage = dataIn.readUTF();
+				if (clientMessage.equals("bye"))
+					break;
+				System.out.println(clientMessage);
+				System.out.print("Enter a line for the Server: ");
+				message = console.readLine();
+			}
+			clientSocket.close();
+		}
+		catch (IOException e) {
+			System.out.println("An error has occured: " + e);
+			System.exit(0);
+		}
+	}
+}
+
+
+
+
+/***********************************************************MY Client*********************************************/
+
+
+
+
+//MyClient:
+
+import java.io.*;
+import java.net.*;
+
+public class MyClient {
+	public static void main(String[] args){
+		String host = "localhost"; // vulcan.seidenberg.pace.edu
+		int port = 16790; 
+		try {
+			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+			Socket clientSocket = new Socket(InetAddress.getByName(host),port);
+			while (true) {
+				DataInputStream dataIn = new DataInputStream(clientSocket.getInputStream());
+				System.out.println(dataIn.readUTF());
+				System.out.print(" Enter a line or a bye to quit from the client:");
+				String message = console.readLine();
+				DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+				out.writeUTF(message);
+			}
+		}
+		catch (EOFException e) {
+			//Connection is closed by server.
+		}
+		catch (IOException e) {
+			System.out.println("An error has occured: " + e);
+			System.exit(0);
+		}
+	}
+}
